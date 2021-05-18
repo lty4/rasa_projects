740dcdec219828f2e4bdd7c5241ca95205a7e333
lty4 <laurence@lty4.com>
2021-05-18 11:08:13 -0500

    Update code

--- inbox_populate/send_to_inbox.py
+++ inbox_populate/send_to_inbox.py
@@ -1,28 +1,34 @@
 import csv
 import requests
+import getpass
 
 
 def send_to_inbox(username, password, link):
 	auth_api_string = link + "/api/auth"
 	auth_json_str = {"username": username, "password": password}
 	auth_token = requests.post(auth_api_string, json=auth_json_str)
+	bearer_token = "Bearer " + auth_token.json()["access_token"]
+	bearer_header = {"Authorization": bearer_token}
+	content_type = {"Content-Type": "application/json"}
+	header_list = {**bearer_header, **content_type}
 	
 	api_string = link + "/api/conversations/"
 	
 	with open('statements.csv', newline='') as statementfile:
 		statementreader = csv.reader(statementfile, delimiter=',')
+		i = 0
 		for row in statementreader:
-			full_api_string = api_string + "test/messages?" + auth_token
-			json_str = {"message": row}
-			conversation = requests.post(full_api_string, json=json_str)
-			print("accepted")
+			i += 1
+			full_api_string = api_string + "import_" + str(i) + "/messages"
+			json_str = {"message": row[0]}
+			conversation = requests.post(url=full_api_string, json=json_str, headers=header_list)
 			
 	return
 
 
 link = input("Please provide the URL to your Rasa X server.\n")
 username = input("Username: ")
-password = input("Password: ")
+password = getpass.getpass("Password: ")
 
 send_to_inbox(username, password, link)
 
